39026e8ad28686cd7ce090e5272c46e628de5048
limon27121 <moontaser009@gmail.com>
2025-07-15 18:10:16 +0600

    update handle_checkboxs section

--- src/test/java/Handle_check_Boxs.java
+++ src/test/java/Handle_check_Boxs.java
@@ -16,11 +16,12 @@ public class Handle_check_Boxs {
         // Open a website
         driver.get("https://testautomationpractice.blogspot.com/");
         driver.manage().window().maximize();
-        //specific checkbox select
+
+        //--specific checkbox select--
 //        driver.findElement(By.xpath("//input[@id='sunday']")).click();
 
 
-        //check all checkboxes
+        //-----check all checkboxes------
 
         List<WebElement>checkboxs=driver.findElements(By.xpath("//input[@class='form-check-input' and @type='checkbox']"));
         System.out.println(checkboxs.size());
@@ -28,12 +29,12 @@ public class Handle_check_Boxs {
 //            element.click();
 //        }
 
-        //check specific checkbox
+        //-----check specific checkbox--------
 //        for (int i=4;i<checkboxs.size();i++){
 //            checkboxs.get(i).click();
 //        }
 
-        //unselected the boxes if they are selected
+        //---unselected the boxes if they are selected
 
         for(int i = 0; i<3; i++){
             checkboxs.get(i).click();
